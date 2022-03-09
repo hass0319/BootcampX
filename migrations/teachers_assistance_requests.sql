f94cc8460000316f5f5c8024be52a642b73ec20c
hass0319 <amro.abdrabou@hotmail.co.uk>
2022-03-09 18:28:12 +0000

    fixes

--- migrations/teachers_assistance_requests.sql
+++ migrations/teachers_assistance_requests.sql
@@ -6,9 +6,10 @@
 -- end_date: The date that the teacher stopped working.
 -- is_active: If the teacher is actively teaching right now.
 -- SELECT (id, name ) FROM teachers;
+
 CREATE TABLE teachers (
   id SERIAL PRIMARY KEY NOT NULL,
-  name TEXT NOT NULL,
+  name VARCHAR(255) NOT NULL,
   is_active BOOLEAN DEFAULT TRUE,
   start_date DATE,
   end_date DATE
@@ -30,12 +31,13 @@ CREATE TABLE teachers (
 
 CREATE TABLE assistance_requests (
   id SERIAL PRIMARY KEY NOT NULL,
-  student_id INTEGER REFERENCES students(id) ON DELETE CASCADE ,
-  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
+  student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
   teacher_id INTEGER REFERENCES teachers(id) ON DELETE CASCADE,
+  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
   created_at TIMESTAMP,
   started_at TIMESTAMP,
   completed_at TIMESTAMP,
   student_feedback TEXT,
   teacher_feedback TEXT
-)
+);
+
