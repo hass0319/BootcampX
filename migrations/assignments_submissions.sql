5baecb9212a3e9617a27482cd532b36587a208bc
hass0319 <amro.abdrabou@hotmail.co.uk>
2022-03-09 03:56:58 +0000

    .

--- migrations/assignments_submissions.sql
+++ migrations/assignments_submissions.sql
@@ -7,12 +7,13 @@ CREATE TABLE assignments (
   duration INTEGER
   );
 
-  CREATE TABLE assignment_submissions ( id SERIAL PRIMARY KEY NOT NULL,
+  CREATE TABLE assignment_submissions (
+  id SERIAL PRIMARY KEY NOT NULL,
   assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
   student_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
   duration INTEGER,
   submission_date DATE
   );
-  
-  SELECT count(*) FROM assignment_submissions;
+
+  -- SELECT count(*) FROM assignment_submissions;
 
